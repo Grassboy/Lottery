584ca84bacca161a785d7d8bf564c5a17c5b0d66
grassboy <grassboy@mail2.is.gy>
2017-01-10 12:28:04 +0800

    追加 checksum 驗證檔案的機制

---
+++ checksum.bat
@@ -0,0 +1,14 @@
+@echo off
+data\fciv checksum.bat -wp -md5 > checksum.txt
+data\fciv data/gift99a.csv -wp -md5 >> checksum.txt
+data\fciv data/gift99b.csv -wp -md5 >> checksum.txt
+data\fciv data/user99.csv -wp -md5 >> checksum.txt
+data\fciv data/ps.csv -wp -md5 >> checksum.txt
+data\fciv javascript/controller.js -wp -md5 >> checksum.txt
+data\fciv javascript/pokemon.drawmode.js -wp -md5 >> checksum.txt
+data\fciv javascript/vr.drawmode.js -wp -md5 >> checksum.txt
+data\fciv index.html -wp -md5 >> checksum.txt
+if exist %1 fciv %1 -wp -md5 >> checksum.txt
+findstr ^[a-z0-9]$ checksum.txt
+pause
+del checksum.txt
